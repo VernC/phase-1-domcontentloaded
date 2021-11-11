48240ad0be2d1f1faf895bb5a2c2a915e336bcef
Candice <candice.vernacchio@gmail.com>
2021-11-11 11:30:50 -0700

    commit tree 9a1fa4bbe5f9d8bec64afc2762566d15d1498a0f

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Your code goes here
+// ====================================
+// This is before the test started
+document.addEventListener("DOMContentLoaded", function () {
+  let p = document.querySelector('p')
+  p.textContent = 'This is really cool!';
+});
+//========================================
+// document.addEventListener("DOMContentLoaded", ready)
+//     function updateText(words) {
+//         let p = document.querySelector('p')
+//         p.textContent = words
+//     }
+
+// updateText('This is really cool!')
+//========================================
+// //works but doesn't work
+// function updateText(words){
+//     let p = document.querySelector('p')
+//     p.textContent = words
+// }
+// updateText('This is really cool!')
+// //=================================
+// //works but doesn't work
+// function updateText(words){
+//     let p = document.querySelector('p')
+//     p.innerText = words
+// }
+// updateText('This is really cool!')
+// //-----------------------------------
